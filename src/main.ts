17502904618a42a26eabf9bb10f6126832b2de64
Adhim Nioka <niokagi@gmail.com>
2024-03-19 06:37:35 +0700

    Morn Commits

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import { AppModule } from './app.module';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 
 async function NestJS() {
-  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule, new FastifyAdapter()
+  );
+  await app.listen(3000, '0.0.0.0');
 }
 
 NestJS();
